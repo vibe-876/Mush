9fc9f3a1fd2f61c06b19f85c8c4c14d660a96d77
cam <ayresc17@holyportcollege.org.uk>
2024-02-24 15:14:21 +0000

    Can format single words, but segfaults at > 1.

--- src/Mush.c
+++ src/Mush.c
@@ -14,15 +14,43 @@ int main(int argc, char *argv[])
     return(0);
 }
 
-
+/* mushLoop ends when only "exit" is typed */
 int mushLoop()
 {
     char *rawCmd = malloc(RCMDSIZE);
+    char **cmd = malloc(RCMDSIZE / CMDSIZE);
 
     do {
 	fgets(rawCmd, RCMDSIZE, stdin);
+	mushFormat(rawCmd, cmd);
     } while(strcmp(rawCmd, "exit\n"));
 
     free(rawCmd);
+    return(0);
+}
+
+int mushFormat(char *raw, char **cmd)
+{
+    int i = 0;
+    int row = 0;
+    int col = 0;
+
+    cmd[col] = malloc(CMDSIZE);
+
+    while(raw[i] != '\n') {
+	switch(raw[i]) {
+	case ' ':
+	    cmd[col][row] = '\0';
+	    printf("%s\n\n\n", cmd[col]);
+	    row = 0;
+	    i++;
+	    cmd[col++] = malloc(CMDSIZE);
+
+	default:
+	    printf("row:\t%d\ncol:\t%d\ni:\t%d\n\n", row, col, i);
+	    cmd[col][row++] = raw[i++];
+	} cmd[col][row] = '\0';
+    }
+
     return(0);
 }
--- src/Mush.h
+++ src/Mush.h
@@ -1,5 +1,7 @@
 /* The maximum length of a input command. */
 #define RCMDSIZE 128
+#define CMDSIZE 16
 
 
 int mushLoop();
+int mushFormat(char *raw, char **cmd);
